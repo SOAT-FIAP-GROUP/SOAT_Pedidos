36537a22eb1a7ae03e72cda76a5e811e372cd0d7
Rogerio Inacio <rogerio.reaper@gmail.com>
2026-01-08 07:22:14 -0300

    test: import class FilaPedidosPreparacaoUseCaseTest unit tests

--- src/test/java/br/com/ms_pedidos/MsPedidosApplicationTests.java
+++ src/test/java/br/com/ms_pedidos/MsPedidosApplicationTests.java
@@ -1,13 +1,13 @@
 package br.com.ms_pedidos;
 
+import br.com.ms_pedidos.usecase.impl.FilaPedidosPreparacaoUseCaseTest;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MsPedidosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+	@Nested
+	class FilaPedidosPreparacaoTests extends FilaPedidosPreparacaoUseCaseTest {}
 }
